130b193cac33c992fc982f7a911a6aa9b59a7d7f
David <dvloedman@protonmail.com>
2021-02-01 15:43:52 -0800

    Setup parameter for /lists/pokemon/[page] so it will offset 20 items by the page number given

--- components/pokemon/ListView.js
+++ components/pokemon/ListView.js
@@ -2,22 +2,20 @@ import Link from "next/link"
 import styles from  "../../styles/List.module.css"
 
 export default function ListView({props}){
-  const pageData = Object.values(props);
-  console.log(pageData)
+  const {count, results} = props;
+  
   return (
     <ul className={styles.indexList}>
       
-      {pageData.map(li => <ListItem item={li} />)}
+      {results.map(li => <ListItem key={li.name} item={li} />)}
     </ul>
   )
 }
 
 const ListItem = ({item}) => {
-  
-
   return (
     <Link href={`/details/pokemon/${item.id}`}>
-      <li key={item.name} className={styles.indexListItem}>
+      <li className={styles.indexListItem}>
       <img src={item.imgUrl} className={styles.thumbnail}/>
       <Capitialized word={item.name} />
       </li>
--- pages/lists/pokemon/[page].js
+++ pages/lists/pokemon/[page].js
@@ -1,5 +1,6 @@
 import ListView from "../../../components/pokemon/ListView";
 import Layout from "../../../components/Layout";
+import Pagination from "react-bootstrap/Pagination"
 
 const POKEMON_ROOT = process.env.api.pokemon;
 
@@ -7,31 +8,56 @@ export default function PokemonList(props) {
   return(
     <Layout>
       <ListView props={props} />
+      <Pagination>
+
+      </Pagination>
     </Layout>
   )
    
 }
 
 export async function getServerSideProps(context) {
-  const url = `${POKEMON_ROOT}`;
-  const res = await fetch(url);
-  const firstData = await res.json();
+  const pageNumber = context.params.page;
 
-  const pageData = await Promise.all(
-    firstData.results.map(async (poke) => {
-      const res = await fetch(poke.url);
-      const secondData = await res.json();
-      return {
-        id: secondData.id,
-        name: secondData.name,
-        imgUrl: secondData.sprites.front_default,
-      };
-    })
-  );
+  let url;
+  
+  if(pageNumber){
+    const offset = 20 * pageNumber;
+    const limit = 20;
+    url = `${POKEMON_ROOT}?offset=${offset}&limit=${limit}`;
+  } else {
+    url = `${POKEMON_ROOT}`;
+  }
 
-  return {
-    props: {
-      ...pageData,
-    },
-  };
+  try{
+    const res = await fetch(url);
+    const firstData = await res.json();
+    
+    const pageCount = parseInt(firstData.count / 20);
+  
+    const pageData = await Promise.all(
+      firstData.results.map(async (poke) => {
+        const res = await fetch(poke.url);
+        const secondData = await res.json();
+        return {
+          id: secondData.id,
+          name: secondData.name,
+          imgUrl: secondData.sprites.front_default,
+        };
+      })
+    );
+  
+    return {
+      props: {
+        currentPage: pageNumber,
+        pageCount: pageCount,
+        results: Object.values(pageData),
+      },
+    };
+
+  }catch(err){
+    return {
+      notFound: true
+    }
+  }
 }
